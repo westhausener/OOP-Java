0f6cbfc85a24a3415d5e133f9e67e046f62ab6d4
Andreas Scheuriker <andreas.scheuriker@gmail.com>
2025-03-12 22:31:26 +0100

    tictactoe with gui

--- src/Tictactoe.java
+++ src/Tictactoe.java
@@ -1,89 +1,111 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Tictactoe {
-    private static char[][] board = new char[3][3];
-    private static char currentPlayer = 'X';
+public class Tictactoe extends JFrame {
+    private static final int BOARD_SIZE = 3;
+    private JButton[][] buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
+    private char currentPlayer = 'X';
 
-    public static void main(String[] args) {
+    public Tictactoe() {
+        setTitle("Tic Tac Toe");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE));
         initializeBoard();
-        printBoard();
-        while (true) {
-            playerMove();
-            printBoard();
-            if (isWinner()) {
-                System.out.println("Player " + currentPlayer + " wins!");
-                break;
-            }
-            if (isBoardFull()) {
-                System.out.println("The game is a tie!");
-                break;
-            }
-            switchPlayer();
-        }
     }
 
-    private static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+    private void initializeBoard() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                buttons[i][j] = new JButton("-");
+                buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 60));
+                buttons[i][j].setFocusPainted(false);
+                buttons[i][j].addActionListener(new ButtonClickListener(i, j));
+                add(buttons[i][j]);
             }
         }
     }
 
-    private static void printBoard() {
-        System.out.println("Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+    private class ButtonClickListener implements ActionListener {
+        private int row;
+        private int col;
+
+        public ButtonClickListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (buttons[row][col].getText().equals("-")) {
+                buttons[row][col].setText(String.valueOf(currentPlayer));
+                if (isWinner()) {
+                    JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+                    resetBoard();
+                } else if (isBoardFull()) {
+                    JOptionPane.showMessageDialog(null, "The game is a tie!");
+                    resetBoard();
+                } else {
+                    switchPlayer();
+                }
             }
-            System.out.println();
         }
     }
 
-    private static void playerMove() {
-        Scanner scanner = new Scanner(System.in);
-        int row, col;
-        while (true) {
-            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
-            row = scanner.nextInt();
-            col = scanner.nextInt();
-            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
-                board[row][col] = currentPlayer;
-                break;
-            } else {
-                System.out.println("This move is not valid");
-            }
-        }
-    }
-
-    private static void switchPlayer() {
+    private void switchPlayer() {
         currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
 
-    private static boolean isWinner() {
+    private boolean isWinner() {
         // Check rows and columns
-        for (int i = 0; i < 3; i++) {
-            if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
-                (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if ((buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
+                 buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
+                 buttons[i][2].getText().equals(String.valueOf(currentPlayer))) ||
+                (buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
+                 buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
+                 buttons[2][i].getText().equals(String.valueOf(currentPlayer)))) {
                 return true;
             }
         }
         // Check diagonals
-        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
-            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+        if ((buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
+             buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+             buttons[2][2].getText().equals(String.valueOf(currentPlayer))) ||
+            (buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
+             buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+             buttons[2][0].getText().equals(String.valueOf(currentPlayer)))) {
             return true;
         }
         return false;
     }
 
-    private static boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
+    private boolean isBoardFull() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (buttons[i][j].getText().equals("-")) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                buttons[i][j].setText("-");
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Tictactoe game = new Tictactoe();
+            game.setVisible(true);
+        });
     }
 }
