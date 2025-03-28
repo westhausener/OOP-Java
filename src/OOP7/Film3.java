ac89bf1b52c934bd633cf52ad9f7be76e5b0e179
Andreas Scheuriker <andreas.scheuriker@gmail.com>
2025-03-28 14:29:45 +0100

    Filmklasse3

---
+++ src/OOP7/Film3.java
@@ -0,0 +1,22 @@
+public class Film3 {
+    private String titel;
+    private String figur;
+    private int jahr;
+
+    public Film3(String titel, String figur, int jahr) {
+        this.titel = titel;
+        this.figur = figur;
+        this.jahr = jahr;
+    }
+
+    public Film3() {
+        this("kein Titel", "Keine Figur", 0);
+    }
+
+    public void print() {
+        System.out.println("Titel: " + titel);
+        System.out.println("Figur: " + figur);
+        System.out.println("Jahr: " + jahr);
+        System.out.println("-----------------------");
+    }
+}
