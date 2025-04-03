12d9c082e1ef140e66973c132733ec42945f6723
Andreas Scheuriker <andreas.scheuriker@gmail.com>
2025-04-03 11:14:09 +0200

    emptyclass

---
+++ OOP8/src/Aufgabe_08_05_empty.java
@@ -0,0 +1,18 @@
+
+public class Aufgabe_08_05_empty {
+
+	public static void main(String[] args) {
+		
+		// Zebra und Pinguin erzeugen
+
+		// polymorphe Methode aufrufen
+		
+	}
+	
+	public static void polymorpheMethode(ZooTier t){
+				
+		// Ausgabe von Name und Gewicht
+	}
+
+}
+
